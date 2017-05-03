03abffcb9bf0d8ed1b43c82f699f7049328af93f
tantruong1104 <tan.truong114@gmail.com>
2017-05-03 14:31:08 +0700

    home

--- app/src/main/java/com/hbbsolution/owner/MainActivity.java
+++ app/src/main/java/com/hbbsolution/owner/MainActivity.java
@@ -2,12 +2,28 @@ package com.hbbsolution.owner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(getResources().getString(R.string.home_home));
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+
+        return super.onCreateOptionsMenu(menu);
+
     }
 }
