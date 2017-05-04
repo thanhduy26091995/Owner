1b9b513a34c3f0fab59c138cc4168405e2f177bc
thanhduy26091995 <buivuthanhduy@gmail.com>
2017-05-04 13:35:43 +0700

    addition

---
+++ app/src/main/java/com/hbbsolution/owner/api/ApiClient.java
@@ -0,0 +1,8 @@
+package com.hbbsolution.owner.api;
+
+/**
+ * Created by buivu on 04/05/2017.
+ */
+
+public class ApiClient {
+}
--- app/src/main/java/com/hbbsolution/owner/utils/FontManager.java
+++ app/src/main/java/com/hbbsolution/owner/utils/FontManager.java
@@ -12,7 +12,7 @@ import android.widget.TextView;
 public class FontManager {
 
     public static final String ROOT = "fonts/",
-            FONTAWESOME = ROOT + "ionicons.ttf";
+            FONTAWESOME = ROOT + "fonts/ionicons.ttf";
 
     public static Typeface getTypeface(Context context, String font) {
         return Typeface.createFromAsset(context.getAssets(), font);
