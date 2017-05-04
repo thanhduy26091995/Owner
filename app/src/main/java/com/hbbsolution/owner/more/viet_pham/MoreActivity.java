2296f043271fe956c7c81e44745567ee9b1de915
anhviet8745 <pham.anh.viet.vlth@gmail.com>
2017-05-04 13:46:11 +0700

    Activity add

---
+++ app/src/main/java/com/hbbsolution/owner/more/viet_pham/MoreActivity.java
@@ -0,0 +1,19 @@
+package com.hbbsolution.owner.more.viet_pham;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.hbbsolution.owner.R;
+
+/**
+ * Created by Administrator on 5/4/2017.
+ */
+
+public class MoreActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+    }
+}
--- app/src/main/java/com/hbbsolution/owner/more/viet_pham/TempActivity.java
+++
@@ -1,8 +0,0 @@
-package com.hbbsolution.owner.more.viet_pham;
-
-/**
- * Created by buivu on 04/05/2017.
- */
-
-public class TempActivity {
-}
