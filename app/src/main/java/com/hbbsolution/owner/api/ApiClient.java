d5d8bc0b3f51d061b983d43336cec64872ce41a1
dobynt <nguyentranbaoduy95@gmail.com>
2017-05-31 14:46:07 +0700

    Duy Nguyen: fix Language + screen payment

--- app/src/main/java/com/hbbsolution/owner/api/ApiClient.java
+++ app/src/main/java/com/hbbsolution/owner/api/ApiClient.java
@@ -19,7 +19,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ApiClient {
 
     public static final String BASE_URL = "https://yukotest123.herokuapp.com/";
-    public static String language = "vi";
+    public static String language = "en";
     public static String token = "0eb910010d0252eb04296d7dc32e657b402290755a85367e8b7a806c7e8bd14b0902e541763a67ef41f2dfb3b9b4919869b609e34dbf6bace4525fa6731d1046";
     private static Retrofit retrofit = null;
 
@@ -31,7 +31,7 @@ public class ApiClient {
 
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
-                    .baseUrl(BASE_URL)
+                    .baseUrl(BASE_URL + language + "/")
                     .client(okHttpClient())
                     .addConverterFactory(GsonConverterFactory.create(gson))
                     .build();
