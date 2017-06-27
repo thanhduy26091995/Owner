2b3c7452f1fdae916b59e4c9ed81fae2f02eb9c0
thanhduy26091995 <buivuthanhduy@gmail.com>
2017-06-27 16:46:37 +0700

    fix owner

--- app/src/main/java/com/hbbsolution/owner/maid_profile/choose_maid/view/ChooseMaidActivity.java
+++ app/src/main/java/com/hbbsolution/owner/maid_profile/choose_maid/view/ChooseMaidActivity.java
@@ -5,7 +5,9 @@ import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -15,7 +17,9 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
@@ -430,5 +434,21 @@ public class ChooseMaidActivity extends AppCompatActivity implements View.OnClic
         } else {
             ShowAlertDialog.showAlert(getResources().getString(R.string.loi_thu_lai), ChooseMaidActivity.this);
         }
+    }
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if (v instanceof EditText) {
+                Rect outRect = new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if (!outRect.contains((int) event.getRawX(), (int) event.getRawY())) {
+                    v.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event);
     }
 }
